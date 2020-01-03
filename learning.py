8bf538f28d9f2074584acd8265c3293c4d034b2b
Bart Voet <bart_voet@telenet.be>
2020-01-03 01:07:01 +0100

    [error] Adding error-feedback

--- learning.py
+++ learning.py
@@ -31,11 +31,17 @@ class Question:
         return True
 
     def hint(self):
-        return self.right[0]
+        return self.left + " => " + ', '.join([str(elem) for elem in self.right])
 
 class QuestionInterface:
     def __init__(self,questions):
+        self.initQuestions = questions
         self.questions = questions.copy()
+        self.errors = []
+        self.counter = 0
+
+    def printRemaining(self):
+        print("({} of {} remaining)".format(self.questionsRemaining(),len(self.initQuestions)))
 
     def ask(self,question):
         return print(question + ": ") 
@@ -44,22 +50,42 @@ class QuestionInterface:
         return input("> ")
 
     def questionsRemaining(self):
-        return len(self.questions) == 0
+        return len(self.questions)
+    
+
+    def checkCounter(self):
+        interrupt_at = 5
+        self.counter = self.counter + 1
+        if self.counter % interrupt_at == 0:
+            clear_console()
+            self.printRemaining()
+            if len(self.errors) > 0:
+                print("Errors")
+            for error in self.errors:
+                print(error.hint())
+            input("Type enter to continue")
 
     def run(self):
         while True:
-            if(self.questionsRemaining()):
+            if(self.questionsRemaining() <= 0):
                 return
-            clear_console()
 
+            self.checkCounter()
+            clear_console()
+            
             random_pic = random.randint(0,len(self.questions)-1)
             item = self.questions[random_pic]
             
             if item.checkAnswer(interface):
                 self.questions.remove(item)
+                if item in self.errors:
+                    self.errors.remove(item)
             else:
-                print("Wrong should be {}".format(item.hint()))
+                if item not in self.errors:
+                    self.errors.append(item)
+                print("Wrong: {}".format(item.hint()))
                 input("Enter to continue")
+        prin("Done!!!")
 
 fileQuestions = []
 
